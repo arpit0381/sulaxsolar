3b17ec9096af99406d7c10cfb27b32a1196f09de
Arpit <arpitbajpai038@gmail.com>
2026-06-16 20:27:33 +0530

    feat: update social links, fix service images, add sitemap.xml and update robots.txt

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,16 +2,9 @@ import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import {
   Mail, Phone, MapPin, Clock,
-  ArrowUpRight, Facebook, Instagram, Youtube,
+  ArrowUpRight, Facebook, Instagram,
   Sun
 } from 'lucide-react';
-
-/* Twitter/X icon */
-const XIcon = () => (
-  <svg viewBox="0 0 24 24" width="14" height="14" fill="currentColor">
-    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.744l7.73-8.835L1.254 2.25H8.08l4.259 5.63 5.905-5.63zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
-  </svg>
-);
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -35,10 +28,8 @@ const Footer = () => {
   ];
 
   const socials = [
-    { Icon: Facebook, href: '#', label: 'Facebook' },
-    { Icon: XIcon, href: '#', label: 'X' },
-    { Icon: Instagram, href: '#', label: 'Instagram' },
-    { Icon: Youtube, href: '#', label: 'YouTube' },
+    { Icon: Facebook, href: 'https://www.facebook.com/SulaxIndustries/', label: 'Facebook' },
+    { Icon: Instagram, href: 'https://www.instagram.com/sulaxsolar/', label: 'Instagram' },
   ];
 
   return (
